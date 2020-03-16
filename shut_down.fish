e0c84c11b6b1a5fefd6b6bbb86f7d9c1c3bdd7e6
Yuto Tanaka <karaage_nennen@icloud.com>
2020-03-17 07:14:47 +0900

    Fixed missing sudo and more

--- shut_down.fish
+++ shut_down.fish
@@ -1,8 +1,14 @@
 function shut_down -d 'An alternative to `shutdown`'
-    set read_confirm "Shutdown?"
+
+    sudo uname >> /dev/null
+    if ! test $status -eq 0
+        return
+    end
+    
+    set read_prompt "Shutdown?"
 
     if read_confirm
         sync; sync; sync;
-        shutdown -h now
+        sudo shutdown -h now
     end
 end
